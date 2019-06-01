ff4daeecb9374b32fc0dffb9760805fea41575e7
Jay Welsh <thealchemicalopus@gmail.com>
2019-06-01 23:00:46 +0200

    include build files

---
+++ client/build/precache-manifest.c28443a955e80197591fb386ab0c0a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284a2133fa80dcd4bfa105d1383021c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6cea29372da4862f9f0",
+    "url": "/static/css/main.141259f7.chunk.css"
+  },
+  {
+    "revision": "5aebff71fa422e6474f8",
+    "url": "/static/js/2.46606e92.chunk.js"
+  },
+  {
+    "revision": "d6cea29372da4862f9f0",
+    "url": "/static/js/main.3cb2ba91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c443b2a34bc258f8d5b8d0180e6b64b",
+    "url": "/static/media/demo-img-0.0c443b2a.jpg"
+  }
+]);
