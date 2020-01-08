8d1dffed05207d9aa3ec788e8db14f4c8a5427e6
Jay Welsh <thealchemicalopus@gmail.com>
2020-01-08 22:49:05 +0200

    build files

---
+++ client/build/precache-manifest.b7a7aafa2a90ccd7081c6bbf590e2ff1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a679a8af3941b06fddd003b7e3ed76e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf2d923d81472d7973a",
+    "url": "/static/css/main.cc699b99.chunk.css"
+  },
+  {
+    "revision": "cd291ae202ca56ee8a33",
+    "url": "/static/js/2.e012613b.chunk.js"
+  },
+  {
+    "revision": "4cf2d923d81472d7973a",
+    "url": "/static/js/main.85548bcd.chunk.js"
+  },
+  {
+    "revision": "a7f15a025510a23cd2f5",
+    "url": "/static/js/runtime-main.9aa64bde.js"
+  },
+  {
+    "revision": "79226bac078ce93a58b74aff1a8a6aa3",
+    "url": "/static/media/ethereum_metamask_chrome.79226bac.png"
+  },
+  {
+    "revision": "06a0bce67ef7682db4c887d1f3dae71f",
+    "url": "/static/media/main_network_metamask.06a0bce6.png"
+  },
+  {
+    "revision": "7c4e9411d23bd9b41d38b094256a3dc3",
+    "url": "/static/media/rinkeby_network_metamask.7c4e9411.png"
+  },
+  {
+    "revision": "dd51ea697fcf3d9a327d34c286af4aef",
+    "url": "/static/media/society0x_transparent_white_thicker.dd51ea69.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.248ee6cd1dd9384fb92694c6c82a56b0.js"
+  "/precache-manifest.b7a7aafa2a90ccd7081c6bbf590e2ff1.js"
 );
 
 self.addEventListener('message', (event) => {
